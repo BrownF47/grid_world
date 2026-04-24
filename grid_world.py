a45345250223372035e5550110c57e59aa24b7fb
BrownF47 <alexbrown47@virginmedia.com>
2026-04-24 15:48:31 +0100

    Grid world loading

--- grid_world.py
+++ grid_world.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 class SPACE():
 
@@ -13,6 +14,12 @@ class SPACE():
             self.space_color = 'red'
         elif self.type == 'gate':
             self.space_color = 'blue'
+
+    def __eq__(self, other):
+        return isinstance(other, SPACE) and self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
     
     def print_space(self):
         print(f"Space: ({self.x}, {self.y}) , {self.type}")
@@ -21,30 +28,79 @@ class SPACE():
         plt.scatter(self.x, self.y, marker='s', facecolors='none', edgecolors=self.space_color, s=1000)
         
 
+class AGENT(): 
+
+    def __init__(self, x, y, type='random'):
+        self.x = x
+        self.y = y
+        self.agent_color = 'green'
+        self.type = type
+        self.valid_moves = [(1,0),(0,1),(-1,0),(0,-1)]
+
+    def print_agent(self):
+        print(f"Agent: ({self.x}, {self.y})")
+        
+    def scatter_agent(self):
+        plt.scatter(self.x, self.y, marker='o', color=self.agent_color, s=200)
+    
+    def choose_move(self):
+        if self.type == 'random':
+            return self.choose_random_move()
+    
+    def choose_random_move(self):
+        move = random.choice(list(self.valid_moves))
+        return move
+
+
+
 
 class GRIDWORLD():
 
     def __init__(self, size: tuple):
         self.size = size
-        self.space_list = []
+        self.spaces = set()
+        self.end = False
 
         for i in range(size[0]):
             for j in range(size[1]):
-                self.space_list.append(SPACE(i, j))
+                self.spaces.add(SPACE(i, j))
+        
+        self.agent_list = [AGENT(0,0)]
                 
     def list_spaces(self):
-        for space in self.space_list:
+        for space in self.spaces:
             space.print_space()
+    
+    def list_agents(self):
+        for agent in self.agent_list:
+            agent.print_agent()
 
-    def visualise_spaces(self):
-        for space in self.space_list:
+    def visualise_grid(self):
+        for space in self.spaces:
             space.scatter_space()
+        for agent in self.agent_list:
+            agent.scatter_agent()
         
         plt.gca().set_aspect('equal', adjustable='box')
         plt.xlim((-1,self.size[0]))
         plt.ylim((-1,self.size[1]))
         plt.show()
+    
+    def move_agent(self, agent: AGENT,  move: tuple):
+        new_x = agent.x + move[0]
+        new_y = agent.y + move[1]
+        new_space = SPACE(new_x, new_y) 
+        if new_space in self.spaces:
+            agent.x = new_x
+            agent.y = new_y
+            if new_space == SPACE(1,1):
+                self.end = True
 
-myGrid = GRIDWORLD((3,5))
+        
+myGrid = GRIDWORLD((2,2))
 myGrid.list_spaces()
-myGrid.visualise_spaces()
+myGrid.list_agents()
+myGrid.visualise_grid()
+while not myGrid.end:
+    myGrid.move_agent(myGrid.agent_list[0], myGrid.agent_list[0].choose_move())
+    myGrid.visualise_grid()
